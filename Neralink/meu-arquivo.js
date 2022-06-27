3616765cf846bb9c9a6dd685a7a91cb787be6085
Eduardo de Oliveira <eduardoolive133@gmail.com>
2022-06-26 22:34:41 -0300

    NLFP ver.01
    Essa é a versão definitiva. Por enquanto.

--- Neralink/meu-arquivo.js
+++ Neralink/meu-arquivo.js
@@ -3,23 +3,50 @@ Missao.addEventListener('mouseenter', entrar)
 Missao.addEventListener('mouseout', sair)
 
 function entrar() {
-    Missao.style.background = "rgb(147,112,219)"
+    Missao.style.background = "rgb(238,130,238)"
 }
 function sair() {
-    Missao.style.background = "rgb(194, 194, 194)"
+    Missao.style.background = "rgb(47, 47, 47)"
 }
 var Banner = window.document.getElementById('banner0')
 Banner.addEventListener('click', clicarB)
 Banner.addEventListener('mouseout', sairB)
 
 function clicarB() {
-    Banner.style.width = "75%"
-    Banner.style.height = "75%"
+    Banner.style.width = "85%"
+    Banner.style.height = "85%"
 }
 function sairB() {
     Banner.style.width = "60%"
     Banner.style.height = "60%"
 }
 
+var perguntas01 = window.document.getElementById('li0')
+perguntas01.addEventListener('mouseenter', entrar1)
+perguntas01.addEventListener('mouseout', sair2)
+function entrar1() {
+    perguntas01.style.background = "rgb(238,130,238)"
+}
+function sair2() {
+    perguntas01.style.background = "rgb(47, 47, 47)"
+}
 
+var perguntas02 = window.document.getElementById('li1')
+perguntas02.addEventListener('mouseenter', entrar3)
+perguntas02.addEventListener('mouseout', sair4)
+function entrar3() {
+    perguntas02.style.background = "rgb(238,130,238)"
+}
+function sair4() {
+    perguntas02.style.background = "rgb(47, 47, 47)"
+}
 
+var perguntas03 = window.document.getElementById('li2')
+perguntas03.addEventListener('mouseenter', entrar5)
+perguntas03.addEventListener('mouseout', sair6)
+function entrar5() {
+    perguntas03.style.background = "rgb(238,130,238)"
+}
+function sair6() {
+    perguntas03.style.background = "rgb(47, 47, 47)"
+}
